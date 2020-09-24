33be3f85e15fb79014c71f3f381dc6bfbc4f052d
HamJinkyoung <wlsrud3803@gmail.com>
2020-09-25 00:35:12 +0900

    request.POST.get('optionlist')를 request.POST['optionlist']로 변경

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,9 +5,9 @@ from myapp.models import Option, Basket
 def index(request):
     options = Option.objects
     if request.method=='POST':
-        if request.POST.get('optionlist'):
+        if request.POST['optionlist']:
             basket=Basket()
-            basket.option_list = request.POST.get('optionlist')
+            basket.option_list = request.POST['optionlist']
             basket.ototal_price = request.POST['ototal']
             basket.save()
             return render(request, 'index.html', {'options':options})
